b6b89d7da613ffcfc7784a01b0579b2ca5be04e6
Nathan Hogg <nathanhogg1223@gmail.com>
2025-06-24 11:00:32 -0500

    Update util.py.save_image and util.py.tensor2im (tensor2im_16bit) to retain 16 bit data during conversion and saving.

--- util/util.py
+++ util/util.py
@@ -6,25 +6,18 @@ from PIL import Image
 import os
 
 
-def tensor2im(input_image, imtype=np.uint8):
-    """"Converts a Tensor array into a numpy image array.
+def tensor2im_16bit(input_image):
+    """"Converts a Tensor array into a 16 bit numpy image array.
 
     Parameters:
         input_image (tensor) --  the input image tensor array
         imtype (type)        --  the desired type of the converted numpy array
     """
-    if not isinstance(input_image, np.ndarray):
-        if isinstance(input_image, torch.Tensor):  # get the data from a variable
-            image_tensor = input_image.data
-        else:
-            return input_image
-        image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
-        if image_numpy.shape[0] == 1:  # grayscale to RGB
-            image_numpy = np.tile(image_numpy, (3, 1, 1))
-        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
-    else:  # if it is a numpy array, do nothing
-        image_numpy = input_image
-    return image_numpy.astype(imtype)
+    image_tensor = input_image.detach().cpu()[0]
+    image_numpy = image_tensor.numpy()
+    image_numpy = np.clip(image_numpy, 0, 1)
+    image_numpy = (image_numpy * 65535.0).astype(np.uint16)
+    return image_numpy
 
 
 def diagnose_network(net, name='network'):
@@ -46,7 +39,7 @@ def diagnose_network(net, name='network'):
     print(mean)
 
 
-def save_image(image_numpy, image_path, aspect_ratio=1.0):
+def save_image(image_numpy, image_path, aspect_ratio=1.0) -> None:
     """Save a numpy image to the disk
 
     Parameters:
@@ -54,7 +47,7 @@ def save_image(image_numpy, image_path, aspect_ratio=1.0):
         image_path (str)          -- the path of the image
     """
 
-    image_pil = Image.fromarray(image_numpy)
+    image_pil = Image.fromarray(image_numpy, mode='I;16').save(image_path)
     h, w, _ = image_numpy.shape
 
     if aspect_ratio > 1.0:
